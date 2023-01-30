d1c6a5a56a438b05f019a90e4618b5130d64fab8
Viktoriia Parkhomenko <55848341+victoriaparkhomenko885@users.noreply.github.com>
2023-01-30 22:53:48 +0200

    Add files via upload

---
+++ Lab_work2/Lab_work2/app/env/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Виктория\Desktop\Projects\Lab_work2\Lab_work2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
