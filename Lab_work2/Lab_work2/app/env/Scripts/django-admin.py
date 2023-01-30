b90825865bfac3466514f27da8059a9b3e51e854
Viktoriia Parkhomenko <55848341+victoriaparkhomenko885@users.noreply.github.com>
2023-01-30 22:52:58 +0200

    Delete Lab_work2/Lab_work2/app/env/Scripts directory

--- Lab_work2/Lab_work2/app/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Виктория\Desktop\Projects\Lab_work2\Lab_work2\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
